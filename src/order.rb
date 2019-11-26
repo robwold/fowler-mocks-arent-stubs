1cf74825bc489be4fff5257698dd90c8ebefc1de
Rob <rold@fleetsmart.co.uk>
2019-11-26 15:12:28 +0000

    both tests pass

--- src/order.rb
+++ src/order.rb
@@ -2,20 +2,17 @@ class Order
   def initialize(brand, quantity)
     @brand = brand
     @quantity = quantity
+    @filled = false
   end
 
-  # Get failing mocha assertions:
-  # Minitest::Assertion: unexpected invocation: #<Mock:warehouse>.verify()
-  # unsatisfied expectations:
-  # - expected exactly once, not yet invoked: #<Mock:warehouse>.remove("Talisker", 50)
-  # - expected exactly once, not yet invoked: #<Mock:warehouse>.has_inventory?("Talisker", 50)
   def fill(warehouse)
-    warehouse.has_inventory?(@brand, @quantity)
-    # yes this is broken
-    warehouse.remove(@brand, @quantity)
+    if warehouse.has_inventory?(@brand, @quantity)
+      warehouse.remove(@brand, @quantity)
+      @filled = true
+    end
   end
 
   def filled?
-    true
+    @filled
   end
 end
